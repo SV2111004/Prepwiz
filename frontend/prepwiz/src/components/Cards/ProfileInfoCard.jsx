b3a3b3552dfdb7b9cc47201fdd28e0cf3e3c897d
SV2111004 <gp.shubhra78@gmail.com>
2025-12-28 14:47:02 +0530

    Fix profile image URL for production

--- frontend/prepwiz/src/components/Cards/ProfileInfoCard.jsx
+++ frontend/prepwiz/src/components/Cards/ProfileInfoCard.jsx
@@ -2,9 +2,10 @@ import { useContext } from "react";
 import { UserContext } from "../../context/userContext";
 import { useNavigate } from "react-router-dom";
 
+const BACKEND_URL = import.meta.env.VITE_API_BASE_URL;
+
 const ProfileInfoCard = () => {
   const { user, clearUser } = useContext(UserContext);
-
   const navigate = useNavigate();
 
   const handleLogout = () => {
@@ -13,19 +14,20 @@ const ProfileInfoCard = () => {
     navigate("/");
   };
 
-  // console.log("image", user.profileImageUrl);
-
-  // console.log("user", user);
-
   return (
     <>
       {user && (
         <div className="flex items-center">
           <img
-            src={user.profileImageUrl || ""}
-            alt=""
-            className="w-11 h-11 bg-gray-300 rounded-full mr-3"
+            src={
+              user.profileImageUrl
+                ? `${BACKEND_URL}${user.profileImageUrl}`
+                : ""
+            }
+            alt="profile"
+            className="w-11 h-11 bg-gray-300 rounded-full mr-3 object-cover"
           />
+
           <div>
             <div className="text-[15px] text-black font-bold leading-3">
               {user.name || ""}
@@ -42,4 +44,5 @@ const ProfileInfoCard = () => {
     </>
   );
 };
+
 export default ProfileInfoCard;
